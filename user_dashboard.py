be4d59b295bda63a418d16604347c36a12255201
KartikJPatil <158796875+KartikJPatil@users.noreply.github.com>
2024-04-02 23:31:57 +0530

    Update user_dashboard.py

--- user_dashboard.py
+++ user_dashboard.py
@@ -1,115 +1,92 @@
-import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk
 import subprocess
 from check_assets import check_assets
-from tkinter import colorchooser
 
-def change_background_color():
-    color = colorchooser.askcolor(title="Choose Background Color")
-    if color[1]:
-        root.configure(bg=color[1])
+class AMS:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Asset Management System")
+        self.root.config(bg="#E3FDFD")
 
-def change_button_color():
-    color = colorchooser.askcolor(title="Choose Button Color")
-    if color[1]:
-        add_assets_btn.config(bg=color[1])
-        check_assets_btn.config(bg=color[1])
-        statistics_btn.config(bg=color[1])
-        help_btn.config(bg=color[1])
-        logout_btn.config(bg=color[1])
-        exit_btn.config(bg=color[1])
+        # Make the window full screen
+        self.root.attributes('-fullscreen', True)
 
-def change_text_color():
-    color = colorchooser.askcolor(title="Choose Text Color")
-    if color[1]:
-        add_assets_btn.config(fg=color[1])
-        check_assets_btn.config(fg=color[1])
-        statistics_btn.config(fg=color[1])
-        help_btn.config(fg=color[1])
-        logout_btn.config(fg=color[1])
-        exit_btn.config(fg=color[1])
+        # Title
+        self.icon_title = PhotoImage(file="images/logo1.png")
+        title = Label(self.root, text="Asset Management System", image=self.icon_title, compound=LEFT, font=("times new roman", 40, "bold"), bg="#71C9CE", fg="white", anchor="w", padx=20)
+        title.pack(fill=X)
 
-def open_statistics():
-    subprocess.Popen(["python", "statistics.py"])
-def logout():
-    root.destroy()
-    subprocess.Popen(["python","user_login.py"])
+        # Button logout
+        btn_logout = Button(self.root, text="Logout", command=self.logout, font=("times new roman", 15, "bold"), bg="#579BB1", cursor="hand2")
+        btn_logout.place(x=1380, y=10, height=50, width=150)
 
-def run_api():
-    subprocess.Popen(["python", "add_assets.py"])
 
-def exit_fullscreen():
-    root.attributes('-fullscreen', False)  # Disable fullscreen
-    root.destroy()  # Destroy the window
+        self.dark_mode_state = False
+        self.dark_mode_btn = Button(self.root, text='🌙', command=self.toggle_dark_mode, font=("times new roman", 20))
+        self.dark_mode_btn.place(x=1490, y=80, width=40, height=40)
 
-# Create main window
-root = tk.Tk()
-root.configure(bg="lightblue")
-root.title("Asset Tracking Management System")
+        # Menu
+        self.MenuLogo = Image.open("images/menu_im.png")
+        self.MenuLogo = self.MenuLogo.resize((200, 200), Image.LANCZOS)
+        self.MenuLogo = ImageTk.PhotoImage(self.MenuLogo)
 
-# Make the window fullscreen
-root.attributes('-fullscreen', True)
-label = tk.Label(root, text="Asset Tracking Management System", font=("times new roman", 45, "underline"))
-label.pack()
+        LeftMenu = Frame(self.root, bd=2, relief=RIDGE, bg="#A6E3E9")
+        LeftMenu.place(x=0, y=70, width=200, height=250)
 
-# Load profile image
-profile_image = tk.PhotoImage(file=r"C:\Users\Admin\Downloads\asset_management_system_django\Asset Tracking management system\images\user.png")
-profile_image = profile_image.subsample(4)
+        lbl_menuLogo = Label(LeftMenu, image=self.MenuLogo)
+        lbl_menuLogo.pack(side=TOP, fill=X)
 
-# Create profile icon
-canvas = tk.Canvas(root, width=100, height=100)
-canvas.pack(padx=60, pady=20, anchor='nw')
-canvas.create_image(50, 50, image=profile_image)
+        lbl_menu = Label(LeftMenu, text="Menu", font=("times new roman", 20), bg="#71C9CE")
+        lbl_menu.pack(side=TOP, fill=X)
 
-# Create frame to hold buttons
-button_frame = tk.Frame(root)
-button_frame.pack(side=tk.LEFT, padx=20, pady=20, anchor='nw')
+        button_frame = Frame(root)
+        button_frame.pack(side=LEFT, padx=2, pady=205, anchor='sw')
 
-# Create menu buttons
-add_assets_btn = tk.Button(button_frame, text="Add Assets", padx=20, pady=10, font=('Arial', 14), command=run_api)
-add_assets_btn.pack(fill=tk.X, padx=10, pady=5)
+        # Create menu buttons
+        add_assets_btn = Button(button_frame, text="Add Assets", padx=22, pady=10, font=('Arial', 14), command=self.run_api)
+        add_assets_btn.pack(fill=X, padx=10, pady=5, anchor='w')
 
-check_assets_btn = tk.Button(button_frame, text="Check Assets", padx=20, pady=10, font=('Arial', 14), command=check_assets)
-check_assets_btn.pack(fill=tk.X, padx=10, pady=5)
+        check_assets_btn = Button(button_frame, text="Check Assets", padx=22, pady=10, font=('Arial', 14), command=check_assets)
+        check_assets_btn.pack(fill=X, padx=10, pady=5, anchor='w')
 
-statistics_btn = tk.Button(button_frame, text="Statistics", padx=20, pady=10, font=('Arial', 14), command=open_statistics)
-statistics_btn.pack(fill=tk.X, padx=10, pady=5)
+        statistics_btn = Button(button_frame, text="Statistics", padx=22, pady=10, font=('Arial', 14), command=self.open_statistics)
+        statistics_btn.pack(fill=X, padx=10, pady=5, anchor='w')
 
-help_btn = tk.Button(button_frame, text="Help!", padx=20, pady=10, font=('Arial', 14))
-help_btn.pack(fill=tk.X, padx=10, pady=5)
+        help_btn = Button(button_frame, text="Help!", padx=22, pady=10, font=('Arial', 14))
+        help_btn.pack(fill=X, padx=10, pady=5, anchor='w')
 
-# Create logout button
-logout_btn = tk.Button(root, text="Logout", padx=5, pady=5, font=('Arial', 14), command=logout)
-logout_btn.place(relx=1.0, rely=0.0, anchor='ne', x=-20, y=60)
+        exit_btn = Button(button_frame, text="Exit", padx=22, pady=10, font=('Arial', 14), command=self.exit_program)
+        exit_btn.pack(fill=X, padx=10, pady=5, anchor='w')
 
-# Create exit button
-exit_btn = tk.Button(root, text="Exit", padx=65, pady=10, font=('Arial', 14), command=exit_fullscreen)
-exit_btn.place(relx=1.0, rely=0.0, anchor='n', x=-1420, y=515)
 
-settings_btn = tk.Button(root, text="⚙️", padx=1, pady=1, font=('Arial', 14), command=lambda: None)
-settings_btn.place(relx=1.0, rely=0.0, anchor='ne', x=-20, y=120)  # Adjust the position as needed
+    def run_api(self):
+        subprocess.Popen(["python", "add_assets.py"])
 
-# Create a frame to hold the submenu buttons
-submenu_frame = tk.Frame(root, bg="lightblue")
-submenu_frame.pack_forget()  # Initially hide the submenu frame
+    def open_statistics(self):
+        subprocess.Popen(["python", "statistics.py"])
 
-# Create submenu buttons
-background_color_btn = tk.Button(submenu_frame, text="Change Background Color", padx=20, pady=10, font=('Arial', 14), command=change_background_color)
-background_color_btn.pack(fill=tk.X, padx=10, pady=5)
+    def logout(self):
+        self.root.destroy()
+        subprocess.Popen(["python", "common_login.py"])
 
-button_color_btn = tk.Button(submenu_frame, text="Change Button Color", padx=20, pady=10, font=('Arial', 14), command=change_button_color)
-button_color_btn.pack(fill=tk.X, padx=10, pady=5)
+    def exit_program(self):
+        root.destroy()  # Close the window
 
-text_color_btn = tk.Button(submenu_frame, text="Change Text Color", padx=20, pady=10, font=('Arial', 14), command=change_text_color)
-text_color_btn.pack(fill=tk.X, padx=10, pady=5)
+    def toggle_dark_mode(self):
+        if self.dark_mode_state:
+            # Light mode
+            self.root.config(bg="#E3FDFD")
+            self.dark_mode_btn.config(text='🌙')
+            self.dark_mode_btn.config(fg="black")
+        else:
+            # Dark mode
+            self.root.config(bg="#2E2E2E")
+            self.dark_mode_btn.config(text='🌙')
+            self.dark_mode_btn.config(fg="white")
+        self.dark_mode_state = not self.dark_mode_state
 
-# Function to show/hide the submenu frame
-def toggle_submenu():
-    if submenu_frame.winfo_ismapped():
-        submenu_frame.pack_forget()
-    else:
-        submenu_frame.pack(side=tk.TOP, padx=10, pady=10)
-
-# Configure the settings button to toggle the visibility of the submenu
-settings_btn.config(command=toggle_submenu)
-
-root.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    obj = AMS(root)
+    root.mainloop()
